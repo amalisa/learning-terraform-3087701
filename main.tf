052ee13dcb009113b6b87f3d81eb786355983837
amalisa <30344522+amalisa@users.noreply.github.com>
2022-12-05 20:36:14 +0800

    Add security group

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ data "aws_vpc" "default" {
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = aws_security_group.blog.id
 
   tags = {
     Name = "Learning Terraform"
@@ -29,38 +29,40 @@ resource "aws_instance" "blog" {
 }
 
 resource "aws_security_group" "blog" {
-  name = "blog"
-  tags = {
-    Terraform = "true"
-  }
-  vpc_id = data.aws_vpc.default.id
+    name = "blog"
+    description = "Allow http and https in and allow all out"
+
+    tags = {
+        Terraform = "true"
+    }
+
+    vpc_id = data.aws_vpc.default.id
+
 }
 
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
+resource "aws_security_group_rule "blog_http_in" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
 }
 
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
+resource "aws_security_group_rule "blog_https_in" {
+  type = "ingress"
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
 }
 
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
+resource "aws_security_group_rule "blog_allow_everything_out" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = -1
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
 }
